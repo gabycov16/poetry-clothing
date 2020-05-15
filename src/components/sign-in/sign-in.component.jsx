aedf90ba8fc43ac404497812cae93e60e2421e5a
Gabs <gaby.cov16@gmail.com>
2020-05-14 22:17:53 -0500

    implemented firebase utils, included ability to store authenticated users into database

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-import "./sign-in.styles.scss";
+
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
+
 import { signInWithGoogle } from "../../firebase/firebase.utils";
+
+import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -16,11 +19,13 @@ class SignIn extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
+
     this.setState({ email: "", password: "" });
   };
 
   handleChange = event => {
     const { value, name } = event.target;
+
     this.setState({ [name]: value });
   };
 
@@ -29,25 +34,26 @@ class SignIn extends React.Component {
       <div className="sign-in">
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
+
         <form onSubmit={this.handleSubmit}>
           <FormInput
             name="email"
             type="email"
+            handleChange={this.handleChange}
             value={this.state.email}
             label="email"
-            handleChange={this.handleChange}
             required
           />
           <FormInput
             name="password"
             type="password"
             value={this.state.password}
-            label="password"
             handleChange={this.handleChange}
+            label="password"
             required
           />
           <div className="buttons">
-            <CustomButton type="submit">Sign in</CustomButton>
+            <CustomButton type="submit"> Sign in </CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignedIn>
               Sign in with Google
             </CustomButton>
